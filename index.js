df96213dd65ee4304178b5ac2e5db91b7f36b9e9
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2022-05-26 10:54:47 +0600

    dicumentation added

--- index.js
+++ index.js
@@ -176,6 +176,19 @@ async function mikrotik_server() {
 
         /**
          * --------------------------------------------------------------------
+         * remove user from database for admin
+         * --------------------------------------------------------------------
+         */
+
+        app.delete('/api/admin/users/:id', verifyUser, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await users.deleteOne(filter);
+            res.send(result);
+        })
+
+        /**
+         * --------------------------------------------------------------------
          * user profile update
          * --------------------------------------------------------------------
          */
