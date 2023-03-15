dfde10bf6cb4a9d1226dc220db2132c392e537bf
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2023-03-15 13:33:10 +0600

    Updateing main branch

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,11 +1,22 @@
 // imports
 import productModel from "../models/productModels.js";
 
+// get all products
+const getProducts = async (req, res) => {
+  try {
+    const products = await productModel.find();
+    return res.status(200).send(products);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
+
 // create new product
 const createProduct = async (req, res) => {
   try {
     const product = new productModel({
-      img: req.file.filename,
+      img: req.body.filename,
       name: req.body.name,
       quantity: req.body.quantity,
       price: req.body.price,
@@ -25,14 +36,21 @@ const createProduct = async (req, res) => {
 const updateProduct = async (req, res) => {
   try {
     const product = await productModel.findById(req.params.id);
+    const { filename, name, quantity, price, min_order, max_order } = req.body;
+    if (!filename || !name || !quantity || !price || !min_order || !max_order) {
+      return res
+        .status(400)
+        .send("Non fields error!, please send a valid data");
+    }
     if (product) {
-      product.img = req.file.filename;
-      product.name = req.body.name;
-      product.quantity = req.body.quantity;
-      product.price = req.body.price;
-      product.min_order = req.body.min_order;
-      product.max_order = req.body.max_order;
+      product.img = filename;
+      product.name = name;
+      product.quantity = quantity;
+      product.price = price;
+      product.min_order = min_order;
+      product.max_order = max_order;
       await product.save();
+      console.log(product);
 
       return res.status(200).json(product);
     } else {
@@ -44,4 +62,16 @@ const updateProduct = async (req, res) => {
   }
 };
 
-export { createProduct, updateProduct };
+// delete products
+const deleteProducts = async (req, res) => {
+  try {
+    const product = await productModel.findById(req.params.id);
+    const result = await product.deleteOne();
+    res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
+
+export { createProduct, deleteProducts, getProducts, updateProduct };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,13 +2,17 @@
 import express from "express";
 import {
   createProduct,
+  deleteProducts,
+  getProducts,
   updateProduct,
 } from "../controllers/productControllers.js";
 
 // router
 const router = express.Router();
 
+router.get("/", getProducts);
 router.post("/", createProduct);
 router.put("/:id", updateProduct);
+router.delete("/:id", deleteProducts);
 
 export default router;
