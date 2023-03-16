f14b47fee97c39c671a0b40fe85d79758a722bab
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2023-03-16 12:14:26 +0600

    Payment id error code removed

--- src/models/orderModels.js
+++ src/models/orderModels.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const orderSchema = mongoose.Schema({
   productID: { type: String, required: [true, "product id required"] },
   amount: { type: Number, required: [true, "amount required"] },
-  paymentID: { type: String, required: false, default: false },
+  paymentID: { type: String, required: false },
   isPaid: { type: Boolean, required: false, default: false },
   isReady: { type: Boolean, required: false, default: false },
   isDelivered: { type: Boolean, required: false, default: false },
