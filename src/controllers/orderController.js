cb70d73aa0f0997cd64865f637183f22697ac76a
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2023-03-17 20:20:17 +0600

    payments controllers created

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,6 +1,9 @@
 // imports
-
+import Stripe from "stripe";
 import orderModel from "../models/orderModels.js";
+const stripe = new Stripe(
+  "pk_test_51L0hxlEZlpATTp01pmVfH39AEz88vRS3gtaq24IKt7ycF15zlpMhZYIslPdUBDv76JJI2LOqh2gs9c5vARhhNRSu00W1WaO6Vd"
+);
 
 // create order
 const createOrder = async (req, res) => {
@@ -23,4 +26,49 @@ const createOrder = async (req, res) => {
   }
 };
 
-export { createOrder };
+// payments controllers
+// create payment intent
+
+const createPayment = async (req, res) => {
+  try {
+    const price = req.body.pay;
+    if (price < 999999) {
+      const amount = price * 100;
+      if (amount) {
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
+        if (paymentIntent.client_secret) {
+          return res
+            .status(200)
+            .send({ clientSecret: paymentIntent.client_secret });
+        } else {
+          return res.status(400).send({ clientSecret: "" });
+        }
+      } else {
+        return res.status(400).send({ clientSecret: "" });
+      }
+    } else {
+      return res.status(400).send({ clientSecret: "" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// save payment information
+const updateOrder = async (req, res) => {
+  try {
+    const order = await orderModel.findById(req.params.id);
+    order.paymentID = req.body.transactionId;
+    order.isPaid = true;
+    await order.save();
+    res.status(200).send(order);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { createOrder, createPayment, updateOrder };
