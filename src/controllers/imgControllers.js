95304067ede50d93e3d46bdb6ee57720b2f4bda6
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2023-04-17 08:18:59 +0600

    Images Invalid Problems Solved

--- src/controllers/imgControllers.js
+++ src/controllers/imgControllers.js
@@ -2,16 +2,20 @@ import path from "path";
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // serve products images
-const productImage = (req, res) => {
-  const imageName = req.params.imageName;
-  const imagePath = path.join(
-    __dirname,
-    "uploads",
-    "images",
-    "products",
-    imageName
-  );
-  res.sendFile(imagePath);
+const productImage = async (req, res) => {
+  try {
+    const imageName = req.params.imageName;
+    const imagePath = path.join(
+      __dirname,
+      "uploads",
+      "images",
+      "products",
+      imageName
+    );
+    return res.sendFile(imagePath);
+  } catch (error) {
+    return res.status(400).send({ message: "Invalid Images Routes!" });
+  }
 };
 
 // serve user profile images
@@ -27,7 +31,7 @@ const userProfile = async (req, res) => {
     );
     res.sendFile(imagePath);
   } catch (error) {
-    console.log(error);
+    return res.status(400).send({ message: "Invalid Images Routes!" });
   }
 };
 
