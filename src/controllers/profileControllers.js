f7992b14c7d959f1fb0b00826bae7cdc8e02c99c
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2025-09-28 09:11:43 +0600

    user routes checked

--- src/controllers/profileControllers.js
+++ src/controllers/profileControllers.js
@@ -3,47 +3,51 @@ import profileModel from "../models/profileModels.js";
 
 // get profile
 const getProfile = async (req, res) => {
-  try {
-    const profile = await profileModel.find({ user: req.params.user });
-    console.log(profile);
-    return res.status(200).send(profile);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const profile = await profileModel.find({ user: req.params.user });
+        console.log(profile);
+        return res.status(200).send(profile);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 // create new profile
 const createProfile = async (req, res) => {
-  try {
-    const filter = { user: req.body.user };
-    const options = { upsert: true };
-    const updateDoc = {
-      $set: {
-        user: req.body.user,
-        name: req.body.name,
-        avatar: `https://manufacturer-server.sabbirmahmud.com/images/profiles/${req.file.filename}`,
-        bio: req.body.bio,
-        address: req.body.address,
-        phone: req.body.phone,
-        education: req.body.education,
-        linkedin: req.body.linkedin,
-        github: req.body.github,
-      },
-    };
+    try {
+        const filter = { user: req.body.user };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: {
+                user: req.body.user,
+                name: req.body.name,
+                avatar: `https://api.manufacturer.sabbirmahmud.com/images/profiles/${req.file.filename}`,
+                bio: req.body.bio,
+                address: req.body.address,
+                phone: req.body.phone,
+                education: req.body.education,
+                linkedin: req.body.linkedin,
+                github: req.body.github,
+            },
+        };
 
-    let profile = await profileModel.findOneAndUpdate(
-      filter,
-      updateDoc,
-      options
-    );
-    if (!profile) {
-      profile = await profileModel.findOneAndUpdate(filter, updateDoc, options);
+        let profile = await profileModel.findOneAndUpdate(
+            filter,
+            updateDoc,
+            options
+        );
+        if (!profile) {
+            profile = await profileModel.findOneAndUpdate(
+                filter,
+                updateDoc,
+                options
+            );
+        }
+
+        res.status(201).send(profile);
+    } catch (error) {
+        console.log(error);
     }
-
-    res.status(201).send(profile);
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 export { createProfile, getProfile };
--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -4,82 +4,84 @@ import userModel from "../models/userModels.js";
 
 // get all user: admin route
 const getUsers = async (req, res) => {
-  try {
-    const users = await userModel.find();
-    return res.status(200).send(users);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const users = await userModel.find();
+        return res.status(200).send(users);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 // make someone admin: admin route
 const makeAdmin = async (req, res) => {
-  try {
-    const user = await userModel.find({ email: req.params.email });
-    if (user) {
-      user[0].is_admin = true;
-      await user[0].save();
-      return res.status(200).send(user);
+    try {
+        const user = await userModel.find({ email: req.params.email });
+
+        if (user) {
+            user[0].is_admin = true;
+            await user[0].save();
+            return res.status(200).send(user);
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 // delete user
 const deleteUser = async (req, res) => {
-  try {
-    const result = await userModel.findByIdAndDelete(req.params.id);
-    console.log(result);
-    return res.status(200).send(result);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const result = await userModel.findByIdAndDelete(req.params.id);
+        console.log(result);
+        return res.status(200).send(result);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 // login
 const login_controller = async (req, res) => {
-  try {
-    const email = req.body.email;
-    if (!email) {
-      return res.status(400).json({
-        message: "nun fill errors! please check your email",
-      });
+    try {
+        const email = req.body.email;
+        if (!email) {
+            return res.status(400).json({
+                message: "nun fill errors! please check your email",
+            });
+        }
+        const filter = { email: email };
+        const user = { email };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: user,
+        };
+        let data = await userModel.findOneAndUpdate(filter, updateDoc, options);
+        if (!data) {
+            data = await userModel.findOneAndUpdate(filter, updateDoc, options);
+        }
+
+        const accessToken = jwt.sign(
+            { email: email },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn: "3d",
+            }
+        );
+        return res.status(200).json({ data, accessToken });
+    } catch (error) {
+        console.log(error);
     }
-    const filter = { email: email };
-    const user = { email };
-    const options = { upsert: true };
-    const updateDoc = {
-      $set: user,
-    };
-    let data = await userModel.findOneAndUpdate(filter, updateDoc, options);
-    if (!data) {
-      data = await userModel.findOneAndUpdate(filter, updateDoc, options);
-    }
-    console.log(data);
-    const accessToken = jwt.sign(
-      { email: email },
-      process.env.ACCESS_TOKEN_SECRET,
-      {
-        expiresIn: "3d",
-      }
-    );
-    return res.status(200).json({ data, accessToken });
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 // user details route
 const userDetails = async (req, res) => {
-  try {
-    const user = await userModel.find({ email: req.query.email });
-    return res.status(200).json({
-      message: user[0] ? user[0].is_admin : false,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const user = await userModel.find({ email: req.query.email });
+
+        return res.status(200).json({
+            message: user[0] ? user[0].is_admin : false,
+        });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export { deleteUser, getUsers, login_controller, makeAdmin, userDetails };
