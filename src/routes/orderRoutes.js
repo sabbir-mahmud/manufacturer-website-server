cbd04f6883476305dabf4e0a24db3587cddf68fb
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2025-09-28 20:08:03 +0600

    Dashboard api issues resolved

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,28 +1,29 @@
 // imports
 import express from "express";
 import {
-  createOrder,
-  createPayment,
-  deleteOrder,
-  getAdminOrders,
-  getOrder,
-  getOrders,
-  shippedOrder,
-  updateOrder,
+    createOrder,
+    createPayment,
+    deleteOrder,
+    getAdminOrders,
+    getOrder,
+    getOrders,
+    shippedOrder,
+    updateOrder,
 } from "../controllers/orderController.js";
 import verifyAdmin from "../middlewares/checkAdmin.js";
+import verifyUser from "../middlewares/checkUser.js";
 
 // router
 const router = express.Router();
 
 // routes
 router.get("/", getOrders);
-router.get("/admin", verifyAdmin, getAdminOrders);
+router.get("/admin", verifyUser, verifyAdmin, getAdminOrders);
 router.get("/:id", getOrder);
 router.post("/", createOrder);
 router.post("/payment", createPayment);
 router.patch("/:id", updateOrder);
-router.patch("/shipped/:id", verifyAdmin, shippedOrder);
+router.patch("/shipped/:id", verifyUser, verifyAdmin, shippedOrder);
 router.delete("/:id", deleteOrder);
 
 export default router;
